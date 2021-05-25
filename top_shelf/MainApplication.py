1297f14171bd06298fc6620096205b9732c2c4e3
Aidan Curley <acurley@financialforce.com>
2021-05-25 18:30:48 +0100

    added sizing to app window

--- top_shelf/MainApplication.py
+++ top_shelf/MainApplication.py
@@ -3,8 +3,11 @@ import tkinter as tk
 class TopShelfApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
+        self.geometry('600x600')
+        tk.Tk.iconbitmap(self,default='')
+        tk.Tk.wm_title(self, "Top Shelf")
 
+        container = tk.Frame(self)
         container.pack(side="top", fill="both", expand = True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
