e38c765d28ebb848f99376a00cfa6d1bec4e459e
Aidan Curley <acurley@financialforce.com>
2021-06-12 17:21:38 +0100

    refactor common methods

--- top_shelf/MainApplication.py
+++ top_shelf/MainApplication.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import Frame, messagebox
 import tkinter.ttk as ttk
 
 class TopShelfApp(tk.Tk):
@@ -19,18 +19,70 @@ class TopShelfApp(tk.Tk):
 
         # set up a dictionary to store the various frames
         self.frames = {}
-
-        self.frames[HomePage] = HomePage(container, self)
-        self.frames[AddBottlePage] = AddBottlePage(container, self)
-        self.frames[EditBottlePage] = EditBottlePage(container, self)
-
-        current_frame = self.frames[AddBottlePage]
-        current_frame.grid(row=0, column=0, sticky=tk.NSEW)
-        self.show_frame(AddBottlePage)
+        # add our pages to tthe frames dictionary
+        for F in (HomePage, AddBottlePage, EditBottlePage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky=tk.NSEW)
+        self.show_frame(HomePage)
 
     def show_frame(self, frame):
         current_frame = self.frames[frame]
         current_frame.tkraise()
+
+    def render_bottle_details_layout(self, frame):
+        frame.distillery_lbl = tk.Label(frame, text="Distillery: ")
+        frame.distillery_lbl.grid(row=1, column=1, columnspan=2, padx=20, pady=(20, 0), sticky=tk.W)
+        frame.distillery_txt = tk.Entry(frame)
+        frame.distillery_txt.grid(row=1, column=3, columnspan=3, pady=(20, 0), sticky=tk.W)
+
+        frame.name_lbl = tk.Label(frame, text="Name: ")
+        frame.name_lbl.grid(row=2, column=1, columnspan=2, padx=20, pady=(10, 0), sticky=tk.W)
+        frame.name_txt = tk.Entry(frame)
+        frame.name_txt.grid(row=2, column=3, columnspan=3, pady=(10, 0), sticky=tk.W)
+
+        frame.age_lbl = tk.Label(frame, text="Age: ")
+        frame.age_lbl.grid(row=3, column=1, columnspan=2, padx=20, pady=(10, 0), sticky=tk.W)
+        frame.age_txt = tk.Entry(frame)
+        frame.age_txt.grid(row=3, column=3, columnspan=3, pady=(10, 0), sticky=tk.W)
+
+        frame.price_lbl = tk.Label(frame, text="Price: ")
+        frame.price_lbl.grid(row=4, column=1, columnspan=2, padx=20, pady=(10, 0), sticky=tk.W)
+        frame.price_txt = tk.Entry(frame)
+        frame.price_txt.grid(row=4, column=3, columnspan=3, pady=(10, 0), sticky=tk.W)
+
+        frame.save_btn = ttk.Button(frame, text="Save Details", command=lambda: self.save_entry(frame))
+        frame.save_btn.grid(row=6, column=1,columnspan=3, padx=(20, 0), pady=(20,0), ipady=5, sticky=tk.NSEW)
+
+        frame.cancel_btn = ttk.Button(frame, text="Cancel", command=lambda: self.cancel_entry(frame))
+        frame.cancel_btn.grid(row=6, column=4,columnspan=3, pady=(20,0), ipady=5, sticky=tk.NSEW)
+
+    def save_entry(self, frame):
+        distillery = frame.distillery_txt.get()
+        name = frame.name_txt.get()
+        age = frame.age_txt.get() if frame.age_txt.get() != "" else "N/A"
+        price = frame.price_txt.get()
+
+        new_bottle = Bottle(distillery, name, age, price)
+        bottles.append(new_bottle)
+        frame.display_bottles()
+        self.clear_entry_boxes(frame)
+        return
+
+    def clear_entry_boxes(self, frame):
+        frame.distillery_txt.delete(0, tk.END)
+        frame.name_txt.delete(0, tk.END)
+        frame.age_txt.delete(0, tk.END)
+        frame.price_txt.delete(0, tk.END)
+        return
+
+    def cancel_entry(self, frame):
+        response = messagebox.askquestion("Confirm", "Are you sure you want to leave without saving?")
+        if response == messagebox.YES:
+            self.clear_entry_boxes(frame)
+            self.show_frame(HomePage)
+        return
+
 
 class Error(Exception):
     """Base class for exceptions in this app"""
@@ -45,8 +97,7 @@ class InputError(Error):
     """
 
     def __init__(self, expression, message):
-        self.expression = expression
-        self.message = message
+        messagebox.showerror(title=expression, message=message)
 
 class Bottle(object):
     def __init__(self, distillery, name, age, price):
@@ -69,17 +120,17 @@ class HomePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
 
-        global bottles
-        bottles = []
+        global bottles, current_bottle
+        bottles = [Bottle("distillery", "name", "1", "1")]
         self.display = tk.Label(self, text="Welcome! You have X bottles in your collection.")
         self.display.grid(column=1,columnspan=6, padx=20, pady=40)
         self.show_col_btn = ttk.Button(self, text="Show Collection", style='W.TButton')
         self.show_col_btn.grid(row=4, column=1,columnspan=3, padx=(20, 0), pady=(20,0), ipady=5, sticky=tk.NSEW)
         self.find_btn = ttk.Button(self, text="Find a Bottle", style='W.TButton')
         self.find_btn.grid(row=4, column=4,columnspan=3, pady=(20,0), ipady=5, sticky=tk.NSEW)
-        self.add_btn = ttk.Button(self, text="Add a Bottle", style='W.TButton')
+        self.add_btn = ttk.Button(self, text="Add a Bottle", style='W.TButton', command = lambda: controller.show_frame(AddBottlePage))
         self.add_btn.grid(row=5, column=1,columnspan=2, padx=(20, 0), pady=(20,0), ipady=5)
-        self.edit_btn = ttk.Button(self, text="Edit a Bottle", style='W.TButton')
+        self.edit_btn = ttk.Button(self, text="Edit a Bottle", style='W.TButton', command = lambda: controller.show_frame(EditBottlePage))
         self.edit_btn.grid(row=5, column=3,columnspan=2, pady=(20,0), ipady=5)
         self.remove_btn = ttk.Button(self, text="Remove a Bottle", style='W.TButton')
         self.remove_btn.grid(row=5, column=5,columnspan=2, pady=(20,0), ipady=5)
@@ -90,56 +141,7 @@ class AddBottlePage(tk.Frame):
 
         global bottles
         controller.wm_title("Add a Bottle")
-        self.distillery_lbl = tk.Label(self, text="Distillery: ")
-        self.distillery_lbl.grid(row=1, column=1, columnspan=2, padx=20, pady=(20, 0), sticky=tk.W)
-        self.distillery_txt = tk.Entry(self)
-        self.distillery_txt.grid(row=1, column=3, columnspan=3, pady=(20, 0), sticky=tk.W)
-
-        self.name_lbl = tk.Label(self, text="Name: ")
-        self.name_lbl.grid(row=2, column=1, columnspan=2, padx=20, pady=(10, 0), sticky=tk.W)
-        self.name_txt = tk.Entry(self)
-        self.name_txt.grid(row=2, column=3, columnspan=3, pady=(10, 0), sticky=tk.W)
-
-        self.age_lbl = tk.Label(self, text="Age: ")
-        self.age_lbl.grid(row=3, column=1, columnspan=2, padx=20, pady=(10, 0), sticky=tk.W)
-        self.age_txt = tk.Entry(self)
-        self.age_txt.grid(row=3, column=3, columnspan=3, pady=(10, 0), sticky=tk.W)
-
-        self.price_lbl = tk.Label(self, text="Price: ")
-        self.price_lbl.grid(row=4, column=1, columnspan=2, padx=20, pady=(10, 0), sticky=tk.W)
-        self.price_txt = tk.Entry(self)
-        self.price_txt.grid(row=4, column=3, columnspan=3, pady=(10, 0), sticky=tk.W)
-
-        self.save_btn = ttk.Button(self, text="Save Details", command=self.save_entry)
-        self.save_btn.grid(row=6, column=1,columnspan=3, padx=(20, 0), pady=(20,0), ipady=5, sticky=tk.NSEW)
-
-        self.cancel_btn = ttk.Button(self, text="Cancel", command=self.cancel_entry)
-        self.cancel_btn.grid(row=6, column=4,columnspan=3, pady=(20,0), ipady=5, sticky=tk.NSEW)
-
-    def save_entry(self):
-        distillery = self.distillery_txt.get()
-        name = self.name_txt.get()
-        age = self.age_txt.get() if self.age_txt.get() != "" else "N/A"
-        price = self.price_txt.get()
-
-        new_bottle = Bottle(distillery, name, age, price)
-        bottles.append(new_bottle)
-        self.display_bottles()
-        self.clear_entry_boxes()
-        return
-
-    def clear_entry_boxes(self):
-        self.distillery_txt.delete(0, tk.END)
-        self.name_txt.delete(0, tk.END)
-        self.age_txt.delete(0, tk.END)
-        self.price_txt.delete(0, tk.END)
-        return
-
-    def cancel_entry(self):
-        response = messagebox.askquestion("Confirm", "Are you sure you want to leave without saving?")
-        if response == messagebox.YES:
-            self.clear_entry_boxes()
-        return
+        controller.render_bottle_details_layout(self)
 
     def display_bottles(self):
         for bottle in bottles:
@@ -149,7 +151,10 @@ class AddBottlePage(tk.Frame):
 class EditBottlePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        controller.wm_title("Add a Bottle")
+
+        global bottles
+        controller.render_bottle_details_layout(self)
+
 
 
 app = TopShelfApp()
